565cf8f11bd2bbb169482594c71a165722bc3867
Anton_Chyrko <anton_chyrko@epam.com>
2022-01-28 14:07:10 +0300

    remove all from main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,4 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int[] arrayEasy = {3, 2, 1};
-
-        bubbleSorting(arrayEasy);
-
-    // write your code here
-
-
-
-
-    }
-
 }
